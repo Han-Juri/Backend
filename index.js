22a37d7ff5e730f84d33a48037ff324026b7d491
Han-Juri <francojoaquinbeccari@gmail.com>
2023-03-08 20:17:29 -0300

    Tercera entrega

--- index.js
+++ index.js
@@ -1,94 +1,116 @@
-const fs = require('fs')
+import fs, { existsSync } from 'fs'
 
-const path = 'Products.json'
-class ProductManager {
-
+export default class ProductManager {
     constructor(path) {
         this.path = path
     }
-    getProducts = async () => {
+
+    async getProducts() {
         if (fs.existsSync(this.path)) {
-            const infoArchivo = await fs.promises.readFile(this.path, 'utf-8')
-            const products = JSON.parse(infoArchivo)
-            return products
+            const products = await fs.promises.readFile(this.path, 'utf-8')
+            return JSON.parse(products)
         } else {
-            console.log('El archivo no existe')
-            console.log([])
+            return []
         }
     }
 
-    addProduct = async (product) => {
-        const products = await this.getProducts()
-        let id
-        if (products.length === 0) {
-            id = 1
-        } else {
-           id = products[products.length - 1].id + 1
-        }
-        const newProduct = { id, ...product }
-        products.push(newProduct)
-        await fs.promises.writeFile(this.path, JSON.stringify(products))
-        return newProduct
-
-    }
-
-    deleteProduct = async (id) => {
-        const products = await this.getProducts()
-        const newList = products.filter(p => p.id !== id)
-        await fs.promises.writeFile(this.path, JSON.stringify(newList))
-            .catch(console.log('User not found'))
-    }
-
-    updateProduct = async (id, obj) => {
-        const products = await this.getProducts()
-        const indexProduct = products.findIndex((p) => p.id === id)
-        if (indexProduct === -1) {
-            return 'Not found'
-        }
-        const productsUpdate = { ...products[indexProduct], ...obj }
-        products.splice(indexProduct, 1, productsUpdate)
-        await fs.promises.writeFile(this.path, JSON.stringify(products))
-    }
-
-    getElementById = async (id) => {
-
-        const products = await this.getProducts()
-        const product = products.find(p => p.id === id)
-        if(product){
+    async getProductsById(idProd) {
+        const productsFile = await this.getProducts()
+        const product = productsFile.find((p) => p.id === idProd)
+        if (product) {
             return product
         } else {
-            console.log('Not found')
+            return 'Not found'
         }
     }
 
+    async addProduct(obj) {
+        const productsFile = await this.getProducts()
+        const id = this.#idGenerator(productsFile)
+        const newProduct = { id, ...obj }
+        productsFile.push(newProduct)
+        await fs.promises.writeFile(this.path, JSON.stringify(productsFile))
+        return newProduct
+    }
+
+    async updateProduct (idProd, obj) {
+        const productsFile = await this.getProducts()
+        const product = productsFile.find((p) => p.id === idProd)
+        if (!product) {
+            return 'Not found'
+        } else {
+            const updatedProduct = { ...product, ...obj }
+            const productIndex = productsFile.findIndex((p) => p.id === idProd)
+            productsFile.splice(productIndex, 1, updatedProduct)
+            await fs.promises.writeFile(this.path, JSON.stringify(productsFile))
+            return 'Product updated'
+        }
+    }
+
+    async deleteProducts() {
+        if (existsSync(this.path)) {
+            await fs.promises.unlink(this.path)
+            return 'All products deleted'
+        } else {
+            return 'Not found'
+        }
+    }
+
+    async deleteProductsById(idProd) {
+        const productsFile =await this.getProducts()
+        const productIndex = productsFile.findIndex((p) => p.id === idProd)
+        if (productIndex === -1) {
+            return 'Not found'
+        } else {
+            productsFile.splice(productIndex, 1)
+            await fs.promises.writeFile(this.path, JSON.stringify(productsFile))
+            return 'Product deleted'
+        }
+    }
+
+    #idGenerator = (products) => {
+		let id = products.length === 0 ? 1 : products[products.length - 1].id + 1;
+		return id;
+	};
 }
 
-const producto1 = {
-    title: 'TV',
-    description: 'Lalalalalooooo',
+const product1 = {
+    title: "TV",
+    description: "Lalalalalooooo",
     price: 200,
-    thumbnail: 'imagen',
-    code: 585858,
-    stock: 10
+    thumbnail: "imagen",
+    code: 584444458,
+    stock: 100
 }
-const producto2 = {
-    title: 'celu',
-    description: 'Lololololaaaaaaaa',
-    price: 2000,
-    thumbnail: 'imagen',
-    code: 5855555555555858,
-    stock: 1000
+const product2 = {
+    title: "Play4",
+    description: "Opaaaa",
+    price: 200000,
+    thumbnail: "imagen",
+    code: 58142538,
+    stock: 101
 }
-
-async function prueba() {
-    const manager = new ProductManager('Products.json')
-    //await manager.getProducts()
-    //await manager.addProduct(producto1)
-    //await manager.addProduct(producto2)
-    /* const productos = await manager.getElementById(5)
-    console.log(productos) */
-    //await manager.updateProduct(4,{title: 'Yahoooo'})
-    //await manager.deleteProduct(2)
+const product3 = {
+    title: "Play5",
+    description: "Ufffffff",
+    price: 400000,
+    thumbnail: "imagen",
+    code: 577858,
+    stock: 1044
 }
-
-prueba()
+const product4 = {
+    title: "Celular",
+    description: "Atendeeeeeeeee",
+    price: 200000,
+    thumbnail: "imagen",
+    code: 5858,
+    stock: 1033
+}
+const product5 = {
+    title: "Auriculares",
+    description: "Taraaaaaaan",
+    price: 200,
+    thumbnail: "imagen",
+    code: 585745378,
+    stock: 10555
+}
